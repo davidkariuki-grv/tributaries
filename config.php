4e5ad77e1d65740cf147028078e7afbd713557d2
David Kariuki <kinyanjuidavidkariuki@gmail.com>
2025-04-04 14:21:53 +0300

    Add files via upload

--- config.php
+++ config.php
@@ -1,15 +1,19 @@
 <?php
-$uri = "mysql://avnadmin:AVNS_ydJBHfECwJzwXbpvjNo@mysql-1700a326-tribute.c.aivencloud.com:25350/tributaries?ssl-mode=REQUIRED";
-$fields = parse_url($uri);
+require 'vendor/autoload.php';
 
-// build the DSN including SSL settings
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+//build the DSN including SSL settings
 $conn = "mysql:";
-$conn .= "host=" . $fields["host"];
-$conn .= ";port=" . $fields["port"];;
+$conn .= "host=" . $_ENV["host"];
+$conn .= ";port=" . $_ENV["port"];;
 $conn .= ";dbname=tributaries";
 $conn .= ";sslmode=verify-ca;sslrootcert='ca.pem'";
+
+
 try {
-    $db = new PDO($conn, $fields["user"], $fields["pass"]);
+    $db = new PDO($conn, $_ENV["user"], $_ENV["pass"]);
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
 }
